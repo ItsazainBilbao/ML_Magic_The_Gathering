06262ea3debab447ba24dc31dcd47aa86949e96e
ItsazainBilbao <itsazainmbilbao@gmail.com>
2025-05-08 17:21:57 +0200

    Segunda subida

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -48,3 +48,90 @@ def clean_color_identity(x):
         return '.i'
     return ','.join(x)
 
+# Tipo de cambio USD -> EUR
+USD_TO_EUR = 0.93
+
+# Convertir string a dict
+def parse_prices(prices_str):
+    try:
+        return ast.literal_eval(prices_str)
+    except:
+        return {}
+
+# Lógica de prioridad de precios
+def get_final_price(prices):
+    eur = prices.get('eur')
+    usd = prices.get('usd')
+    eur_foil = prices.get('eur_foil')
+    usd_foil = prices.get('usd_foil')
+    usd_etched = prices.get('usd_etched')
+
+    if eur and eur != 'None':
+        return float(eur)
+    
+    if usd and usd != 'None':
+        try:
+            return round(float(usd) * USD_TO_EUR, 2)
+        except:
+            pass
+
+    if eur_foil and eur_foil != 'None':
+        return float(eur_foil)
+
+    if usd_foil and usd_foil != 'None':
+        try:
+            return round(float(usd_foil) * USD_TO_EUR, 2)
+        except:
+            pass
+
+    if usd_etched and usd_etched != 'None':
+        try:
+            return round(float(usd_etched) * USD_TO_EUR, 2)
+        except:
+            pass
+
+    return None
+
+# Formatos que nos interesan
+target_formats = ['standard', 'pioneer', 'modern', 'legacy', 'vintage', 'commander', 'pauper']
+
+# Función para convertir string a dict
+def parse_legalities(legalities_str):
+    try:
+        return ast.literal_eval(legalities_str)
+    except:
+        return {}
+
+# Función para mapear legalidad a número
+def map_legal_status(status):
+    if status == 'legal':
+        return 1
+    elif status == 'not_legal':
+        return 0
+    elif status == 'banned':
+        return -1
+    else:
+        return None  # por si aparece algo inesperado
+    
+    
+from collections import Counter
+
+def contar_subtipos(columna, sep=','):
+    """
+    Recibe una Serie de pandas (columna del DataFrame),
+    separa las etiquetas por sep (coma por defecto), y cuenta cuántas veces aparece cada una.
+    
+    Devuelve un DataFrame ordenado por frecuencia descendente.
+    """
+    # Asegurar valores string y separar por coma
+    etiquetas = columna.dropna().astype(str).apply(lambda x: [t.strip() for t in x.split(sep) if t.strip()])
+    
+    # Aplanar y contar
+    contador = Counter([etiqueta for sublist in etiquetas for etiqueta in sublist])
+    
+    # Convertir a DataFrame
+    df_frecuencias = pd.DataFrame.from_dict(contador, orient='index', columns=['frecuencia']).sort_values(by='frecuencia', ascending=False)
+    df_frecuencias.index.name = 'subtipo'
+    df_frecuencias.reset_index(inplace=True)
+    
+    return df_frecuencias
